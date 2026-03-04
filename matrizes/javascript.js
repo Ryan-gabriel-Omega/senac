878a1af03758b7be060182e7582395221577cb61
Ryan Gabriel Ferreira De Lima <deskblek@gmail.com>
2026-03-04 08:31:21 -0300

    .

--- matrizes/javascript.js
+++ matrizes/javascript.js
@@ -1,53 +1,22 @@
 const matriz = [
-    ["0, 0", "0, 1", "0, 2"],
-    ["1, 0", "1, 1", "1, 2"],
-    ["2, 0", "2, 1", "2, 2"]
+    [1, 7, 2, 5],
+    [8, 45, 82, 1],
+    [2, 7, 3, 4],
+    [10, 17, 14, 12]
 ];
 
-const grade = document.getElementById('grade')
-
-const log = document.getElementById('log')
-
-matriz.forEach((linha, i) => {
-    linha.forEach((valor, j) => {
-        const div = document.createElement('div')
-        div.classList.add('celula')
-        div.id = `cel-${i}-${j}`
-        div.innerText = valor
-        grade.appendChild(div)
-
-    });
-});
-
-
-
-for (let i = 0; j < matriz.length; i++) {
-    
-    for (let j = 0; j < matriz[i].length; j++) {
-        const id = `cel-${i}-${j}`;
-        const elementoHtml = document.getElementById(id);
-    }
-
-}
-
-
+let resultado = "";
 
 for (let i = 0; i < matriz.length; i++) {
-
-    // FOR INTERNO: Percorre as COLUNAS daquela linha
+    
     for (let j = 0; j < matriz[i].length; j++) {
-        const id = `cel-${i}-${j}`;
-        const elementoHtml = document.getElementById(id);
-
-        // Destacar célula atual
-        elementoHtml.classList.add('ativo');
-        log.innerHTML += `Lendo: Linha ${i}, Coluna ${j} -> <strong>${matriz[i][j]}</strong><br>`;
-
-        await sleep(1800); // Pausa para explicação visual
-
-        elementoHtml.classList.remove('ativo');
+        
+        resultado += matriz[i][j] + " ";
+        
     }
-    log.innerHTML += `--- Fim da linha ${i} ---<br>`;
+
+    resultado += "\n";
 }
-log.innerHTML += "<strong>Fim do percurso!</strong>";
-}
+
+
+document.getElementById("saida").innerText = resultado;
