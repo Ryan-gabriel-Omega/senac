85e7eb50aaefac1de5469a16235010020b6d6d97
Ryan Gabriel Ferreira De Lima <deskblek@gmail.com>
2026-04-27 12:02:10 -0300

    .

--- PHP/self_static.php
+++ PHP/self_static.php
@@ -1,9 +1,28 @@
 <?php
- 
-class teste {
-    public static $nomes = "mateus<br>joao";
+
+class Produto {
+    public static $tipo = 'geral';
+
+    public static function getTipoSelf() {
+        return self::$tipo;
+    }
+
+    public static function getTipoStatic() {
+        return static::$tipo;
+    }
 }
 
-echo teste::$nomes
+class Eletronico extends Produto {
+    public static $tipo = 'eletrônico';
+}
+
+// Teste
+echo 'Self: ';
+echo Eletronico::getTipoSelf();
+
+echo '<br>';
+
+echo 'Static: ';
+echo Eletronico::getTipoStatic();
 
 ?>
