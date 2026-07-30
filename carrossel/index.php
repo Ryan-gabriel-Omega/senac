35205a681bb8c600a6497dc74b998206e3691c9a
Ryan Gabriel Ferreira De Lima <deskblek@gmail.com>
2026-07-30 15:47:55 -0300

    .

---
+++ carrossel/index.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>carrossel</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="carrossel">
+
+            <button id="anterior"></button>
+
+            <div class="slides">
+                
+                <?php foreach ($... as $... => $...):?>
+                    <img src=""<?= $...?>"class="slide">
+                <?php  endforeach; ?>
+
+            <button id="proximo"></button>
+            </div>
+    </div>
+    
+    <script src="script.js"></script>
+    
+</body>
+</html>
